02d5c459389c8e3b8643fb28871c2a2041d79968
Frank Wilkerson <FWilkerson@ad.exetered.com>
2018-01-06 20:39:20 -0700

    new view poll result page

---
+++ packages/client/src/views/view_poll_result.js
@@ -0,0 +1,11 @@
+import {h} from '../lib/muve';
+
+export default model => (
+	<section>
+		<div class="row">
+			<div class="columns eight offset-by-two">
+				<p>Placeholder</p>
+			</div>
+		</div>
+	</section>
+);

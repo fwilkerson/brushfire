ff90788cf4aab2667ca92e1af9a0559bc0fdddd1
Frank Wilkerson <FWilkerson@ad.exetered.com>
2018-02-01 23:06:35 -0700

    remove old setup

--- packages/utils/index.js
+++
@@ -1,8 +0,0 @@
-exports.count = (iterable, predicate) => {
-	if (predicate == null || typeof iterable == 'string') return iterable.length;
-	return iterable.filter(predicate).length;
-};
-
-exports.isNullOrWhiteSpace = str => {
-	return str == null || str.trim() === '';
-};
